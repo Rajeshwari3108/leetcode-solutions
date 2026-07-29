8611fb9ce9b278f96251127d4977eb890c508407
RAJESHWARI S <rajismilychinnu@gmail.com>
2026-07-29 18:59:39 +0530

    Time: 30 ms (88.56%), Space: 59.4 MB (32.61%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,10 +4,10 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
+
             int j = i + 1;
             int k = nums.length - 1;
 
@@ -20,14 +20,17 @@ class Solution {
                     j++;
                 } else {
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    
                 }
             }
         }
-        return res;        
+
+        return res;
     }
 }

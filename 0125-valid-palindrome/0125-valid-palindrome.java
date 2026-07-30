53ff3dd7a50976a462c9c7d1e72a67c297970bd1
RAJESHWARI S <rajismilychinnu@gmail.com>
2026-07-30 17:18:56 +0530

    Time: 2 ms (99.33%), Space: 44.4 MB (64.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-
         while (left < right) {
 
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
